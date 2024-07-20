101905964e07753d95a5baef39aaaee58a43576c
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-07-20 12:32:09 +0100

    Implement User POST, GET, DELETE, and PUT routes

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,53 +8,9 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/all_users.yml')
-def get_users():
-    """
-    Retrieves the list of all user objects
-    or a specific user
-    """
-    all_users = storage.all(User).values()
-    list_users = []
-    for user in all_users:
-        list_users.append(user.to_dict())
-    return jsonify(list_users)
-
-
-@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/get_user.yml', methods=['GET'])
-def get_user(user_id):
-    """ Retrieves a user by its ID """
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-
-    return jsonify(user.to_dict())
-
-
-@app_views.route('/users/<user_id>', methods=['DELETE'],
-                 strict_slashes=False)
-@swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
-def delete_user(user_id):
-    """
-    Deletes a user Object
-    """
-
-    user = storage.get(User, user_id)
-
-    if not user:
-        abort(404)
-
-    storage.delete(user)
-    storage.save()
-
-    return make_response(jsonify({}), 200)
-
-
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/user/post_user.yml', methods=['POST'])
-def post_user():
+@app_views.route('/register', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/user/register_user.yml')
+def register_user():
     """
     Creates a user
     """
@@ -76,13 +32,60 @@ def post_user():
 
         # Commit the session to the database
         storage.save()
-        return make_response(jsonify(instance.to_dict()), 201)
+        # return make_response(jsonify(instance.to_dict()), 201)
+        return make_response(jsonify(message="Ok"), 201)
     except IntegrityError:
-        abort(400, description="Email already exists")
+        return make_response(jsonify(message="Email already exists"), 409)
 
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/all_users.yml')
+def get_users():
+    """
+    Retrieves the list of all user objects
+    or a specific user
+    """
+    all_users = storage.all(User).values()
+
+    if not all_users:
+        return make_response(jsonify(message="No users found"), 404)
+
+    list_users = []
+    for user in all_users:
+        list_users.append(user.to_dict())
+    return jsonify(list_users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/get_user.yml')
+def get_user(user_id):
+    """ Retrieves a user by its ID """
+    user = storage.get(User, user_id)
+    if not user:
+        return make_response(jsonify(message="No user found"), 404)
+
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/user/delete_user.yml')
+def delete_user(user_id):
+    """
+    Deletes a user Object
+    """
+
+    user = storage.get(User, user_id)
+
+    if not user:
+        return make_response(jsonify(message="No user found"), 404)
+
+    storage.delete(user)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/user/put_user.yml', methods=['PUT'])
+@swag_from('documentation/user/put_user.yml')
 def put_user(user_id):
     """
     Updates a user
@@ -90,7 +93,7 @@ def put_user(user_id):
     user = storage.get(User, user_id)
 
     if not user:
-        abort(404)
+        return make_response(jsonify(message="No user found"), 404)
 
     if not request.get_json():
         abort(400, description="Not a JSON")
@@ -102,4 +105,4 @@ def put_user(user_id):
         if key not in ignore:
             setattr(user, key, value)
     storage.save()
-    return make_response(jsonify(user.to_dict()), 200)
+    return make_response(jsonify(message="User updated"), 200)
