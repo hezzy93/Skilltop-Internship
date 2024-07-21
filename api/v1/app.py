1c7ed905ab7297e78c92e406f488915ac0271e8d
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-07-21 14:42:26 +0100

    Refactor code

--- api/v1/app.py
+++ api/v1/app.py
@@ -41,10 +41,7 @@ Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('IMS_API_HOST')
-    port = environ.get('IMS_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = environ.get('IMS_API_HOST', '0.0.0.0')
+    port = environ.get('IMS_API_PORT', '5000')
+
     app.run(host=host, port=port, threaded=True, debug=True)
